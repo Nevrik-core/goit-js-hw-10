d33d4f6ffcfe1f8a6733701a64a3b81e18b08184
Nevrik-core <57769617+Nevrik-core@users.noreply.github.com>
2022-08-18 14:36:02 +0300

    made country info markup

--- src/index.js
+++ src/index.js
@@ -1,4 +1,57 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
+
+const form = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
 const DEBOUNCE_DELAY = 300;
+
+form.addEventListener('input', debounce(onTypeCountryName, DEBOUNCE_DELAY));
+
+function onTypeCountryName(e) {
+    const city = e.target.value.trim();
+    console.log(city);
+    fetchCountries(city).then(data => {
+        if (data.length > 10) {
+           Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+        } else if (data.length === 1) {
+            console.log(data.length);
+            createInfoMarkup(data[0]);
+        }
+        
+    });
+        
+}
+
+function createInfoMarkup(obj) {
+    
+    const name = obj.name.official
+    const capital = obj.capital[0];
+    const flagIcon = obj.flag;
+    const population = obj.population;
+    const languages = Object.values(obj.languages).map(el => el).join(', ');
+    const flag = obj.flags.svg;
+    console.log(obj);
+    
+    countryInfo.innerHTML = `<div class="main">
+        <img src="${flag}" alt="${name}${flagIcon}" height="30" />
+        <h1>${name}</h1>
+      </div>
+      <ul>
+        <li>
+          <b>Capital:</b>
+          ${capital}
+        </li>
+        <li><b>Population:</b> ${population}</li>
+        <li><b>Languages:</b> ${languages}</li>
+      </ul>`
+}
+
+
+
+
+
+
